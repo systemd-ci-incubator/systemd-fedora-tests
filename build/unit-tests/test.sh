d2ec4b522f5eb3636d949d9b37b65e695a2802f1
Frantisek Sumsal <frantisek@sumsal.cz>
2021-03-25 19:14:36 +0100

    Fall back to the `systemd-tests` package
    Rebuilding the systemd package takes currently too much time, so let's
    run the (a bit limited) set of unit tests provided via the
    `systemd-tests` package.

--- build/unit-tests/test.sh
+++ build/unit-tests/test.sh
@@ -3,12 +3,20 @@
 set -eux
 set -o pipefail
 
-TEMPDIR="$(mktemp -d)"
-pushd "$TEMPDIR"
+if ! rpm -q systemd-tests; then
+    echo >&2 "Missing package 'systemd-tests'"
+    exit 1
+fi
 
-trap "rm -fr '$TEMPDIR'" EXIT
+/usr/lib/systemd/tests/run-unit-tests.py --unsafe
 
-dnf download --source "$(rpm -q systemd)"
-dnf -y builddep systemd*.src.rpm
-dnf -y install gnu-efi-devel
-rpmbuild --clean --recompile systemd*.src.rpm
+## This currently takes ages, so let's put it aside for now
+#TEMPDIR="$(mktemp -d)"
+#pushd "$TEMPDIR"
+#
+#trap "rm -fr '$TEMPDIR'" EXIT
+#
+#dnf download --source "$(rpm -q systemd)"
+#dnf -y builddep systemd*.src.rpm
+#dnf -y install gnu-efi-devel
+#rpmbuild --clean --recompile systemd*.src.rpm
